bf97529a922ccaf73c0220a5db5dec66c817ebce
iamrajatverma <rajat.verma@jarvis.consulting>
2019-06-22 13:42:47 +0530

    migration added

---
+++ database/migrations/2019_06_22_051659_create_task_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaskUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task_users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('task_id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('assigned_id');
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task_users');
+    }
+}
